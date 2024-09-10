615103ce3218d1734b7f97f66d6a629e63e3f8bc
NittaShinji <poki4pocky@gmail.com>
2024-09-10 19:33:55 +0900

    IEnemyクラスを作成し、出現させて弾を撃てるように

--- 10DaysBike/EnemyManager.cpp
+++ 10DaysBike/EnemyManager.cpp
@@ -14,7 +14,7 @@ void EnemyManager::Update()
 	// 敵の削除（デスフラグの立ったものを削除）
 	enemies_.erase(
 		std::remove_if(enemies_.begin(), enemies_.end(),
-		[](std::unique_ptr<Enemy>& enemy) {
+		[](std::unique_ptr<IEnemy>& enemy) {
 		return enemy->IsDead();
 	}),
 	enemies_.end());
@@ -26,13 +26,14 @@ void EnemyManager::Update()
 		{
 			if (enemies_.size() < kMaxEnemyNum)
 			{
-				GenerateEnemy();
+				GenerateTriangleEnemy();
+				GenerateBulletEnemy();
 				waitTimer_ = kWaitTime_;
 			}
 		}
 	}
 
-	for (std::unique_ptr<Enemy>& enemy : enemies_)
+	for (std::unique_ptr<IEnemy>& enemy : enemies_)
 	{
 		enemy->Update();
 	}
@@ -42,7 +43,7 @@ void EnemyManager::Update()
 
 void EnemyManager::Draw()
 {
-	for (std::unique_ptr<Enemy>& enemy : enemies_)
+	for (std::unique_ptr<IEnemy>& enemy : enemies_)
 	{
 		enemy->Draw();
 	}
@@ -50,7 +51,7 @@ void EnemyManager::Draw()
 	bulletManager_->Draw();
 }
 
-void EnemyManager::GenerateEnemy()
+void EnemyManager::GenerateBulletEnemy()
 {
 	//乱数シード生成器
 	std::random_device seed_gen;
@@ -80,6 +81,44 @@ void EnemyManager::GenerateEnemy()
 	//敵を生成し、初期化
 	std::unique_ptr<Enemy> newEnemy = std::make_unique<Enemy>();
 	newEnemy->Init(enemyPopPos);
+
+	newEnemy->SetBullletManger(bulletManager_.get());
+
+	//敵を登録する
+	enemies_.push_back(std::move(newEnemy));
+}
+
+void EnemyManager::GenerateTriangleEnemy()
+{
+	//乱数シード生成器
+	std::random_device seed_gen;
+	//メルセンヌツイスターの乱数エンジン
+	std::mt19937_64 engine(seed_gen());
+	//乱数範囲の指定
+	std::uniform_int_distribution<int> dist(0, 2);
+
+	// ランダムな値を取得
+	int randomValue = dist(engine);
+
+	// 敵の位置をランダムで生成
+	Vec2 popPos;
+
+	if (randomValue == Up)
+	{
+		popPos = { WINDOW_SIZE.x / 2 ,WINDOW_SIZE .y / 2};
+	}
+	else if (randomValue == Left)
+	{
+		popPos = { WINDOW_SIZE.x / 2 ,WINDOW_SIZE.y / 2 };
+	}
+	else
+	{
+		popPos = { WINDOW_SIZE.x / 2 ,WINDOW_SIZE.y / 2 };
+	}
+
+	//敵を生成し、初期化
+	std::unique_ptr<TriangleEnemy> newEnemy = std::make_unique<TriangleEnemy>();
+	newEnemy->Init(popPos);
 
 	newEnemy->SetBullletManger(bulletManager_.get());
 
--- 10DaysBike/IEnemy.h
+++ 10DaysBike/IEnemy.h
@@ -12,12 +12,15 @@ public:
 	static const uint16_t PROT_ENEMY_DRAWING_SIZE = 40;
 	static const ColorDxLib PROT_ENEMY_COLOR;
 	static const int TURN_KEY = KEY_INPUT_SPACE;
-private:
-	
+
+protected:
+
 	//死亡フラグ
 	bool isDead_ = false;
 	//当たったかどうか
 	bool isHit_ = false;
+	//ダメージをどれだけ受けるか
+	uint16_t damageNum_ = 1;
 	//敵のコライダー
 	std::unique_ptr<CircleCollider> enemyCollider_ = nullptr;
 	//HP
@@ -29,10 +32,15 @@ public:
 
 public:
 	void Init()override;
+	void Init(const Vec2& pos);
 	void Update()override;
 	void Draw()override;
 
 public:
+
+	//コライダーの追加
+	void AddCollider();
+
 	//衝突時に呼び出される関数
 	void OnCollision(const CollisionInfo& info) override;
 
