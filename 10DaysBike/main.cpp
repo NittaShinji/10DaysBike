6ac6fbcb4e16b3072e81ee4c68d86e9fab5d742e
NittaShinji <poki4pocky@gmail.com>
2024-09-13 02:11:42 +0900

    戻せてなかったのでゲームから始まるよう戻した

--- 10DaysBike/main.cpp
+++ 10DaysBike/main.cpp
@@ -49,7 +49,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	SceneManager::GetInstance()->SetSceneFactory(std::move(sceneFactory_));
 	//シーンマネージャに最初のシーンをセット
 	SceneManager::GetInstance()->Initialize();
-	SceneManager::GetInstance()->ChangeScene("TITLE");
+	SceneManager::GetInstance()->ChangeScene("GAME");
 	GameScene::StaticInitialize();
 
 	// ゲームループ
